b536292dff4b71ac67e000c62a7eb4da5cb4a550
Sujoy Das <sujoykarnel@gmail.com>
2024-07-03 15:31:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+      <h2>Home Add</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+]);
